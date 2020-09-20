a5d5d347f50f94416b6e656b35904871323aa073
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-20 11:49:31 +0000

    Deploy codinggirl/letscontribute to github.com/codinggirl/letscontribute.git:gh-pages

---
+++ README.md
@@ -0,0 +1,6 @@
+Notes:
+
+Here is a gh-pages hosted static files for <https://codinggirl.github.io/letscontribute/>.
+
+If you are looking for the source of this repo,
+head to <https://github.com/codinggirl/letscontribute-web>.
---
+++ precache-manifest.1328804570cf5955d4ad8364a6822a55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe64ecdeda37697c5e9a288cbf85eba",
+    "url": "/letscontribute/index.html"
+  },
+  {
+    "revision": "38099c5ea895a3ef176e",
+    "url": "/letscontribute/static/css/main.97cb4511.chunk.css"
+  },
+  {
+    "revision": "9a8075a838dcc3220d41",
+    "url": "/letscontribute/static/js/2.33ab520e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/letscontribute/static/js/2.33ab520e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38099c5ea895a3ef176e",
+    "url": "/letscontribute/static/js/main.7624de23.chunk.js"
+  },
+  {
+    "revision": "a2487a4a8fb1c9f3f2f6",
+    "url": "/letscontribute/static/js/runtime-main.c70bf818.js"
+  }
+]);
