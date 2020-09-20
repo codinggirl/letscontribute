8d8e0b9935dd770402aa0944f42d446e6d0154fc
Richard Libre MR <meng@codinggirl.net>
2020-09-20 16:49:27 +0800

    Updates

---
+++ README.md
@@ -0,0 +1,6 @@
+Notes:
+
+Here is a gh-pages hosted static files for <https://1stcontribution.github.io>.
+
+If you are looking for the source of this repo,
+head to <https://github.com/1stcontribution/1stcontribution-web>.
---
+++ precache-manifest.7f41ece0d5d3ec36bb9e0886ce337fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4703acd1840953dd3102afe61f6c3e1b",
+    "url": "/letscontribute/index.html"
+  },
+  {
+    "revision": "a836d44511d4cb67acda",
+    "url": "/letscontribute/static/css/2.699efa4f.chunk.css"
+  },
+  {
+    "revision": "21c64a4fb8ef89cd72d1",
+    "url": "/letscontribute/static/css/main.71e8392b.chunk.css"
+  },
+  {
+    "revision": "a836d44511d4cb67acda",
+    "url": "/letscontribute/static/js/2.c92864a6.chunk.js"
+  },
+  {
+    "revision": "21c64a4fb8ef89cd72d1",
+    "url": "/letscontribute/static/js/main.409672da.chunk.js"
+  },
+  {
+    "revision": "ae082217ab5167275460",
+    "url": "/letscontribute/static/js/runtime-main.ec482aa5.js"
+  }
+]);
